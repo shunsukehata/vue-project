cd99f6c0ffd31adb495bc5aee36f6e2b5350deb4
DESKTOP-H1PEDVE\Shunsuke_Hata <hatajapan.0710@gmail.com>
2024-09-27 14:55:47 +0900

    first commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TodoInput from './TodoInput.vue';
+import TodoListView from './TodoListView.vue';
+</script>
+
+<template>
+    <div class="todoApp">
+    <TodoInput></TodoInput>
+    <TodoListView></TodoListView>  
+    </div>
+
+</template>
---
+++ src/components/TodoListView.vue
@@ -0,0 +1,29 @@
+<script setup>
+const items = JSON.parse(localStorage.getItem("items")) || [];
+</script>
+
+
+<template>
+
+    <div>
+        <table>
+            <tr>
+                <th class="th-id">ID</th>
+                <th class="th-value">やること</th>
+                <th class="th-limit">期限</th>
+                <th class="th-state">状態</th>
+                <th class="th-edit">編集</th>
+                <th class="th-delete">削除</th>
+            </tr>
+            <tr v-for="item in items" :key="item.id">
+                <td>{{ item.id }}</td>
+                <td>{{ item.content }}</td>
+                <td>{{ item.limit }}</td>
+                <td>{{ item.state.value }}</td>
+                <td><button>編集</button></td>
+                <td><button>削除</button></td>
+            </tr>
+        </table>
+    </div>
+
+</template>
